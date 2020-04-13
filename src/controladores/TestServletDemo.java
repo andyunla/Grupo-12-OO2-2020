c033c0bbe1b343ad1c7f29f861bb76fcf2d245ee
andyunla <andy.dev.alnu@hotmail.com>
2020-04-13 15:54:26 -0300

    Small changes

--- src/controladores/TestServletDemo.java
+++ src/controladores/TestServletDemo.java
@@ -9,22 +9,22 @@ public class TestServletDemo extends HttpServlet {
    private String mymsg;
 
    public void init() throws ServletException {
-        mymsg = "Hello World!";
+     mymsg = "Hello World!";
    }
 
    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Setting up the content type of webpage
-        response.setContentType("text/html");
+     // Setting up the content type of webpage
+     response.setContentType("text/html");
 
-        // Writing message to the web page
-        PrintWriter out = response.getWriter();
-        out.println("<h1>" + mymsg + "</h1>");
+     // Writing message to the web page
+     PrintWriter out = response.getWriter();
+     out.println("<h1>" + mymsg + "</h1>");
    }
 
    public void destroy() {
-        /* leaving empty for now this can be
-        * used when we want to do something at the end
-        * of Servlet life cycle
-        */
+     /* leaving empty for now this can be
+     * used when we want to do something at the end
+     * of Servlet life cycle
+     */
    }
 }
