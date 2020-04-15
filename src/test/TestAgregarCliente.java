f8bf25d4c5fe94a29a9e5faaee6ab9b332834b1b
andyunla <andy.dev.alnu@hotmail.com>
2020-04-14 22:47:18 -0300

    Agregado TestAgregarCliente

---
+++ src/test/TestAgregarCliente.java
@@ -0,0 +1,29 @@
+package test;
+import java.time.LocalDate;
+
+import negocio.PersonaABM;
+
+/*
+ * Test para agregar un nuevo cliente
+ */
+public class TestAgregarCliente {
+	public static void main(String[] args) {
+		PersonaABM abm = PersonaABM.getInstance();
+		long idPersona = 1;
+		try {
+			idPersona = abm.agregar(idPersona);
+			String apellido = "Downey Jr";
+			String nombre = "Robert";
+			int dni = 34000001;
+			LocalDate fechaDeNacimiento = LocalDate.now();
+			String email = "robert.downeyjr@gmail.com";
+			int nroCliente = 1;
+
+			System.out.println("Agregar un nuevo cliente \n\n");
+			long ultimoIdCliente = abm.agregar(apellido, nombre, dni, fechaDeNacimiento);
+			System.out.println("Agregado satisfactoriamente. ID: " + Long.toString(ultimoIdCliente));
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
