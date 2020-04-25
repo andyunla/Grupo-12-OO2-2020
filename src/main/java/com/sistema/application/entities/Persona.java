3e4fcef645135f3a768b320f3c39e3e7e1de5e2d
andyunla <andy.dev.alnu@hotmail.com>
2020-04-24 22:45:15 -0300

    Agregado a la entidad Persona como superclase

--- src/main/java/com/sistema/application/entities/Persona.java
+++ src/main/java/com/sistema/application/entities/Persona.java
@@ -9,13 +9,14 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-@Entity
+@MappedSuperclass
 @Table(name="persona")
 public class Persona {
 	@Id
