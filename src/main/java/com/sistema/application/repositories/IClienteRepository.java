fc5544c986a034ae7ffdd21afac5544aa31c1aa4
andyunla <andy.dev.alnu@hotmail.com>
2020-05-02 18:03:30 -0300

    Agregado IClienteRepository.java

---
+++ src/main/java/com/sistema/application/repositories/IClienteRepository.java
@@ -0,0 +1,17 @@
+package com.sistema.application.repositories;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.sistema.application.entities.Cliente;
+
+@Repository("clienteRepository")
+public interface IPersonaRepository extends JpaRepository<Cliente, Serializable> {
+	public abstract Cliente findByNroCliente(int nroCliente);
+	public abstract List<Cliente> findByNombreAndApellido(String nombre, String apellido);
+}
