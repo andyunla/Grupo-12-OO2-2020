65efce813884c5626fb0f8dc7040dc3fc5e873fe
andyunla <andy.dev.alnu@hotmail.com>
2020-05-02 17:18:33 -0300

    Actualizado algunas rutas de vistas

--- src/main/java/com/sistema/application/helpers/ViewRouteHelper.java
+++ src/main/java/com/sistema/application/helpers/ViewRouteHelper.java
@@ -2,6 +2,17 @@ package com.sistema.application.helpers;
 
 public class ViewRouteHelper {
 	/**** Views ****/
-	//HOME
-	public final static String INDEX = "index";
+	// HOME
+	public final static String INDEX = "home/index";
+
+	// ABMs
+	public final static String CLIENTE_ABM = "abm/clientes";
+	public final static String EMPLEADO_ABM = "abm/empleados";
+	public final static String PRODUCTO_ABM = "abm/productos";
+
+	/**** Redirects ****/
+	public final static String HOME_ROOT = "index";
+	public final static String CLIENTE_ROOT = "clientes";
+	public final static String EMPLEADO_ROOT = "empleados";
+	public final static String PRODUCTO_ROOT = "productos";
 }
