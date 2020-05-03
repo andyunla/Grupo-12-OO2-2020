73f653176a185bab9bb85d89d90041885986a1ea
Cristian Villafane <cristianv129@hotmail.com>
2020-05-03 14:26:22 -0300

    rutas agregadas a clase helper

--- src/main/java/com/sistema/application/helpers/ViewRouteHelper.java
+++ src/main/java/com/sistema/application/helpers/ViewRouteHelper.java
@@ -4,4 +4,9 @@ public class ViewRouteHelper {
 	/**** Views ****/
 	//HOME
 	public final static String INDEX = "index";
+	// Products
+	public final static String PRODUCTOS = "abm/productos";
+	// Clients
+	public final static String CLIENTES = "abm/clientes";
+
 }
