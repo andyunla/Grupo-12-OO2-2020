fdb1569c569aeecf5549a332910773b4cba148d5
leanjlg <leanjlg@gmail.com>
2020-05-03 19:06:05 -0300

    Mas repositorios (a completar)

---
+++ src/main/java/com/sistema/application/repositories/ILocalRepository.java
@@ -0,0 +1,16 @@
+package com.sistema.application.repositories;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.sistema.application.entities.Local;
+
+public interface ILocalRepository extends JpaRepository<Local, Serializable>{
+	public abstract Local findByIdLocal(long idLocal);
+	public abstract Local findByNombreLocal(String nombreLocal);
+	public abstract Local findByDireccion(String direccion);
+}
