af51cfd069ddab12b976ea3e5d0b1e51e1476337
Franco2900 <franco2900@gmail.com>
2020-05-07 18:29:35 -0300

    Falta las variables para cuando se llama el return

--- src/main/java/com/sistema/application/converters/PedidoStockConverter.java
+++ src/main/java/com/sistema/application/converters/PedidoStockConverter.java
@@ -1,5 +1,18 @@
 package com.sistema.application.converters;
 
+import com.sistema.application.models.PedidoStockModel;
+import com.sistema.application.entities.PedidoStock;
+
 public class PedidoStockConverter {
 
+	//De entidad a modelo
+	public PedidoStockModel entityToModel(PedidoStock pedidoStock) {
+		return new PedidoStockModel();
+	}
+	
+	//De modelo a entidad
+	public PedidoStock modelToEntity(PedidoStockModel pedidoStockModel) {
+		return new PedidoStock();
+	}
+	.
 }
