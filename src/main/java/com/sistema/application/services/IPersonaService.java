fc8af24d9052772a041d85493c28180cfbadead9
Franco2900 <franco2900@gmail.com>
2020-05-07 23:46:21 -0300

    Acomode el ProductoService que estaba mal ubicado. Puse solo los 3 metodos basicos a cada service getall, insertar/modificar y eliminar

--- src/main/java/com/sistema/application/services/IPersonaService.java
+++ src/main/java/com/sistema/application/services/IPersonaService.java
@@ -5,12 +5,11 @@ import com.sistema.application.models.PersonaModel;
 
 import java.util.List;
 
-public interface IPeronsaService {
+public interface IPersonaService {
 
 	public List<Persona> getAll();
 	
 	public PersonaModel ingresarOActualizar(PersonaModel personaModel);
 	
 	public boolean eliminar(long id);
-	
 }
