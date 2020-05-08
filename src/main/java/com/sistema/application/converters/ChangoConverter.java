e315d98163d43b80e97c4c9493f00dcd0ea32948
leanjlg <leanjlg@gmail.com>
2020-05-07 21:46:58 -0300

    completando converter chango

--- src/main/java/com/sistema/application/converters/ChangoConverter.java
+++ src/main/java/com/sistema/application/converters/ChangoConverter.java
@@ -7,12 +7,12 @@ public class ChangoConverter {
 
 	//De entidad a modelo
 	public ChangoModel entityToModel(Chango chango) {
-		return new ChangoModel(chango.getIdChango(), chango.getListaItems(), chango.getPedidostock().getIdPedidoStock() );
+		return new ChangoModel( chango.getIdChango(), chango.getListaItems(),chango.getPedidostock().getIdPedidoStock());
 	}
 	
 	//De modelo a entidad
 	public Chango modelToEntity(ChangoModel changoModel) {
-		return new Chango(changoModel);
+		return new Chango(changoModel.getIdPedidostock(), changoModel.getIdPedidostock());
 	}
 	
 }
