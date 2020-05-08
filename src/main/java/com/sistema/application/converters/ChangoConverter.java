3d8d04c6964cee4d27a386c460de80f19fca9ed2
leanjlg <leanjlg@gmail.com>
2020-05-08 15:19:42 -0300

    cambios en changoConverter

--- src/main/java/com/sistema/application/converters/ChangoConverter.java
+++ src/main/java/com/sistema/application/converters/ChangoConverter.java
@@ -3,19 +3,21 @@ package com.sistema.application.converters;
 import com.sistema.application.models.ChangoModel;
 import com.sistema.application.models.ItemModel;
 import com.sistema.application.entities.Chango;
-
+import com.sistema.application.entities.Item;
 
 import java.util.Set;
 
 public class ChangoConverter {
-	
+	ItemConverter itemConverter = null;
 	//De entidad a modelo
 	public ChangoModel entityToModel(Chango chango) {
+		// tengo que crear una lista  de itemModel para agregar al changoModel
 		Set<ItemModel> list = null;
-		for (ItemModel itemModel : list) {
-			
+		// a cada item de esta lista tengo que convertirlo de entity a model y agregarlo a la lista
+		for (Item item : chango.getListaItems()) {
+			ItemModel itemModel = itemConverter.entityToModel(item);
+			list.add(itemModel);
 		}
-
 		return new ChangoModel( chango.getIdChango(), list,chango.getPedidostock().getIdPedidoStock());
 	}
 	
