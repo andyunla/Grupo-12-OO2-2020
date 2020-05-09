e9c06e60a80f3f8c7890b0a429a878cfad096203
andyunla <andy.dev.alnu@hotmail.com>
2020-05-09 13:05:28 -0300

    Terminado converter de Empleado

--- src/main/java/com/sistema/application/converters/EmpleadoConverter.java
+++ src/main/java/com/sistema/application/converters/EmpleadoConverter.java
@@ -1,20 +1,27 @@
 package com.sistema.application.converters;
 
-import com.sistema.application.models.EmpleadoModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
 import com.sistema.application.entities.Empleado;
+import com.sistema.application.models.EmpleadoModel;
 
+@Component("empleadoConverter")
 public class EmpleadoConverter {
-
-	//De entidad a modelo
+	@Autowired
+	@Qualifier("localConverter")
+	private static LocalConverter localConverter;
+	
 	public EmpleadoModel entityToModel(Empleado empleado) {
 		return new EmpleadoModel(empleado.getIdPersona(), empleado.getNombre(), empleado.getApellido(), empleado.getDni(), empleado.getFechaNacimiento(),
 								 empleado.getLegajo(), empleado.getHoraDesde(), empleado.getHoraHasta(), empleado.getSueldoBasico(),
-								 empleado.getLocal().getIdLocal(), empleado.getLocalOwner().getIdLocal());
+								 localConverter.entityToModel(empleado.getLocal()), localConverter.entityToModel(empleado.isTipoEmpleado()));
 	}
-
-	//De modelo a entidad
+	
 	public Empleado modelToEntity(EmpleadoModel empleadoModel) {
 		return new Empleado(empleadoModel.getId(), empleadoModel.getNombre(), empleadoModel.getApellido(), empleadoModel.getDni(), empleadoModel.getFechaNacimiento(),
-							empleadoModel.getLegajo(), empleadoModel.getHoraDesde(), empleadoModel.getHoraHasta(), empleadoModel.getSueldoBasico());
+							empleadoModel.getLegajo(), empleadoModel.getHoraDesde(), empleadoModel.getHoraHasta(), empleadoModel.getSueldoBasico(),
+							localConverter.modelToEntity(empleadoModel.getLocal()), localConverter.modelToEntity(empleadoModel.isTipoEmpleado()));
 	}
 }
