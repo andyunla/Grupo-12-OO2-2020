2d8caf33ae4da3884080b7835cfc53250104ba80
leanjlg <63562330+leanjlg@users.noreply.github.com>
2020-05-10 20:53:35 -0300

    completando en LoteConverter

--- src/main/java/com/sistema/application/converters/LoteConverter.java
+++ src/main/java/com/sistema/application/converters/LoteConverter.java
@@ -1,8 +1,13 @@
 package com.sistema.application.converters;
 
 import com.sistema.application.models.LoteModel;
+import com.sistema.application.models.ProductoModel;
 import com.sistema.application.repositories.IProductoRepository;
 
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.sistema.application.entities.Lote;
@@ -10,19 +15,20 @@ import com.sistema.application.entities.Producto;
 
 @Component("loteConverter")
 public class LoteConverter {
-	
+	@Autowired
+	@Qualifier("productoConverter")
+	private ProductoConverter productoConverter;
 	//De entidad a modelo
 	public LoteModel entityToModel(Lote lote) {
 		return new LoteModel(lote.getIdLote(), lote.getCantidadInicial(), lote.getCantidadActual(),
-				lote.getFechaIngreso(), lote.getProducto().getIdProducto(), lote.isActivo() );
+				lote.getFechaIngreso(),productoConverter.entityToModel( lote.getProducto()), lote.isActivo() );
 	}
 	
 	//De modelo a entidad
 	public Lote modelToEntity(LoteModel loteModel) {
-		IProductoRepository ipr = null;
-		Producto prod = ipr.findByIdProducto(loteModel.getIdProducto());
-		return new Lote(loteModel.getCantidadInicial(), loteModel.getCantidadActual(), 
-				loteModel.getFechaIngreso(), prod );
+		
+		return new Lote(loteModel.getIdLote(), loteModel.getCantidadInicial(), loteModel.getCantidadActual(), 
+				loteModel.getFechaIngreso(), productoConverter.modelToEntity(loteModel.getProductoModel()) );
 	}
 	
 }
