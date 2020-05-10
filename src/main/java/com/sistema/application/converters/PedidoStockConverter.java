1f8f3979673b6c5be92373afbe74a48442098ea8
leanjlg <63562330+leanjlg@users.noreply.github.com>
2020-05-10 20:29:34 -0300

    completando en PedidoStockConverter

--- src/main/java/com/sistema/application/converters/PedidoStockConverter.java
+++ src/main/java/com/sistema/application/converters/PedidoStockConverter.java
@@ -4,6 +4,8 @@ import com.sistema.application.models.PedidoStockModel;
 import com.sistema.application.repositories.IEmpleadoRepository;
 import com.sistema.application.repositories.IProductoRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.sistema.application.entities.Empleado;
@@ -12,12 +14,15 @@ import com.sistema.application.entities.Producto;
 
 @Component("pedidoStockConverter")
 public class PedidoStockConverter {
-
+	@Autowired
+	@Qualifier("productoConverter")
+	private ProductoConverter productoConverter;
 	//De entidad a modelo
 	public PedidoStockModel entityToModel(PedidoStock pedidoStock) {
-		//revisar el oferente, solicitante. También el isAceptado()
-		return new PedidoStockModel(pedidoStock.getIdPedidoStock(), pedidoStock.getProducto().getIdProducto(), pedidoStock.getCantidad(), pedidoStock.getSolicitante().getLegajo(), pedidoStock.isAceptado(),
-				pedidoStock.getOferente().getLegajo());
+		//revisar el oferente, solicitante. También el isAceptado() LEANDRO
+		return new PedidoStockModel(pedidoStock.getIdPedidoStock(),productoConverter.entityToModel(pedidoStock.getProducto()), 
+				pedidoStock.getCantidad(), pedidoStock.isAceptado(), pedidoStock.getSolicitanteLegajo(),
+				pedidoStock.getOferenteLegajo());
 	}
 	
 	//De modelo a entidad
