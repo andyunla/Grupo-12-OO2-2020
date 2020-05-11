465e07011b45a45b39ce1de055c4ab0a1f535cc8
leanjlg <63562330+leanjlg@users.noreply.github.com>
2020-05-10 21:05:13 -0300

    completando en ItemConverter

--- src/main/java/com/sistema/application/converters/ItemConverter.java
+++ src/main/java/com/sistema/application/converters/ItemConverter.java
@@ -1,7 +1,10 @@
 package com.sistema.application.converters;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import com.sistema.application.models.ChangoModel;
 import com.sistema.application.models.ItemModel;
 import com.sistema.application.entities.Item;
 
@@ -12,16 +15,21 @@ import com.sistema.application.repositories.IProductoRepository;
 
 @Component("itemConverter")
 public class ItemConverter {
-
+	@Autowired
+	@Qualifier("productoConverter")
+	private ProductoConverter productoConverter;
+	@Autowired
+	@Qualifier("changoConverter")
+	private ChangoConverter changoConverter;
 	
-	//De entidad a modelo
+	//De entidad a modelo long idItem, int cantidad, ProductoModel productoModel, ChangoModel changoModel
 	public ItemModel entityToModel(Item item) {
-		return new ItemModel(item.getIdItem(), item.getCantidad(), item.getProducto().getIdProducto() );
+		return new ItemModel(item.getIdItem(), item.getCantidad(),productoConverter.entityToModel(item.getProducto()) , changoConverter.entityToModel(item.getChango())  );
 	}
 	
 	//De modelo a entidad
 	public Item modelToEntity(ItemModel itemModel) {
-		return new Item();
+		return new Item(itemModel.getIdItem(), itemModel.getCantidad(), productoConverter.modelToEntity(itemModel.getProductoModel()), changoConverter.modelToEntity(itemModel.getChangoModel()));
 	}
 	
 }
