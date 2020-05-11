d091f04cba73635c7c17026812c3c5e4f8cef72c
leanjlg <63562330+leanjlg@users.noreply.github.com>
2020-05-10 21:39:33 -0300

    empleadoConvereter

--- src/main/java/com/sistema/application/converters/EmpleadoConverter.java
+++ src/main/java/com/sistema/application/converters/EmpleadoConverter.java
@@ -1,19 +1,25 @@
 package com.sistema.application.converters;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.sistema.application.entities.Empleado;
 import com.sistema.application.models.EmpleadoModel;
+import com.sistema.application.models.LocalModel;
 
 @Component("empleadoConverter")
 public class EmpleadoConverter {
 	@Autowired
 	@Qualifier("localConverter")
-	private static LocalConverter localConverter;
+	private LocalConverter localConverter;
 	
 	public EmpleadoModel entityToModel(Empleado empleado) {
+//		long id, String nombre, String apellido, int dni, LocalDate fechaNacimiento, 
+//		 int legajo, LocalTime horaDesde, LocalTime horaHasta, double sueldoBasico, LocalModel local, boolean tipoEmpleado
 		return new EmpleadoModel(empleado.getIdPersona(), empleado.getNombre(), empleado.getApellido(), empleado.getDni(), empleado.getFechaNacimiento(),
 								 empleado.getLegajo(), empleado.getHoraDesde(), empleado.getHoraHasta(), empleado.getSueldoBasico(),
 								 localConverter.entityToModel(empleado.getLocal()), empleado.isTipoEmpleado());
