f8c9af94d5938efb51a2b68547a26f0102d02508
leanjlg <63562330+leanjlg@users.noreply.github.com>
2020-05-10 21:25:03 -0300

    cambios en facturaConverter

--- src/main/java/com/sistema/application/converters/FacturaConverter.java
+++ src/main/java/com/sistema/application/converters/FacturaConverter.java
@@ -11,7 +11,9 @@ import com.sistema.application.entities.Cliente;
 import com.sistema.application.entities.Empleado;
 import com.sistema.application.entities.Factura;
 import com.sistema.application.entities.Local;
+import com.sistema.application.models.ChangoModel;
 import com.sistema.application.models.ClienteModel;
+import com.sistema.application.models.EmpleadoModel;
 import com.sistema.application.models.FacturaModel;
 import com.sistema.application.repositories.IClienteRepository;
 
@@ -20,21 +22,27 @@ public class FacturaConverter {
 	@Autowired
 	@Qualifier("clienteConverter")
 	private static ClienteConverter clienteConverter;
-	
+	@Autowired
+	@Qualifier("empleadoConverter")
+	private static EmpleadoConverter empleadoConverter;
 	@Autowired
 	@Qualifier("changoConverter")
 	private static ChangoConverter changoConverter;
-	
+	@Autowired
+	@Qualifier("localConverter")
+	private static LocalConverter localConverter;
 	public FacturaModel entityToModel(Factura factura) {
-		//return new FacturaModel(factura.getIdFactura(), clienteConverter.entityToModel(factura.getCliente()), changoConverter.entityToModel(factura.getChango()), factura.getFechaFactura(), factura.getCosteTotal(),
-		//		factura.getEmpleado().getLegajo());
-		return new FacturaModel();
+		return new FacturaModel(factura.getIdFactura(), clienteConverter.entityToModel(factura.getCliente()),
+				changoConverter.entityToModel(factura.getChango()), factura.getFechaFactura(), factura.getCosteTotal()
+				,empleadoConverter.entityToModel(factura.getEmpleado()), localConverter.entityToModel(factura.getLocal()) );
 	}
 	
 	public Factura modelToEntity(FacturaModel facturaModel) {
 		//return new Factura(clienteConverter.modelToEntity(facturaModel.getCliente()), changoConverter.modelToEntity(facturaModel.getChango()), facturaModel.getFechaFactura(), facturaModel.getCosteTotal(), 
 		//				   clienteConverter.modelToEntity(facturaModel.getEmpleado(), clienteConverter.modelToEntity(facturaModel.getLocal())));
-		return new Factura();
+		return new Factura(facturaModel.getIdFactura(),clienteConverter.modelToEntity(facturaModel.getCliente()),
+				changoConverter.modelToEntity(facturaModel.getChango()), facturaModel.getFechaFactura(), facturaModel.getCosteTotal(),
+				empleadoConverter.modelToEntity(facturaModel.getEmpleado()), localConverter.modelToEntity(facturaModel.getLocal()));
 	}
 	
 }
