8b4ccf113b12648bd49cb66fd29ce230a72994a9
leanjlg <leanjlg@gmail.com>
2020-05-18 19:16:10 -0300

    agrego traer el set de facturas entre fechas de un local

--- src/main/java/com/sistema/application/repositories/IFacturaRepository.java
+++ src/main/java/com/sistema/application/repositories/IFacturaRepository.java
@@ -2,6 +2,7 @@ package com.sistema.application.repositories;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +13,6 @@ import com.sistema.application.entities.Factura;
 @Repository("facturaRepository")
 public interface IFacturaRepository  extends JpaRepository<Factura, Serializable>{
 	public abstract Factura findByIdFactura(long idFactura);
+	@Query("FROM Factura  WHERE id_local = :idLocal AND fecha_factura BETWEEN :fecha1 AND :fecha2")
+	public abstract Set<Factura> findFacturasEntreFechasLocal(LocalDate fecha1, LocalDate fecha2, long idLocal);
 }
