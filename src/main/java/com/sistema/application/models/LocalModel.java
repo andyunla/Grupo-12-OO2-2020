8a958f0c618cbfbcd176bc6d3974cd3158210e77
leanjlg <leanjlg@gmail.com>
2020-05-18 18:28:59 -0300

    agrego changos a LocalModel

--- src/main/java/com/sistema/application/models/LocalModel.java
+++ src/main/java/com/sistema/application/models/LocalModel.java
@@ -317,11 +317,7 @@ public class LocalModel {
 		if(chango.getPedidoStock() == null) restarChango(chango); // Si no hay pedidoStock, resto todos los productos del chango a este local
 		return true;
 	}
-	public void restarChango(ChangoModel chango) {
-		for ( ItemModel it : chango.getListaItems()) {
-			restarLote(it.getProductoModel() , it.getCantidad());
-		}
-	}
+	
 //	public List<Factura> traerFactura (LocalDate fecha1, LocalDate fecha2) {
 //		List<Factura> list = new ArrayList<Factura>();		
 //			int i= 0;
@@ -332,4 +328,23 @@ public class LocalModel {
 //		return list;
 //	}
 
+	/****************************************************************************************************/
+	public ChangoModel crearChango () {
+		return iChangoService.insertOrUpdate(new ChangoModel(this)); //creo un chango nuevo para este Local
+	}
+	public boolean eliminarChango (ChangoModel chango) {// si elimino el chango debo restaurar todos los items
+		sumarChango(chango);// sumo todos los items al local
+		iChangoService.remove(chango.getIdChango());// elimino el chango de la DB
+		return true; // return true
+	}
+	public void restarChango(ChangoModel chango) {
+		for ( ItemModel it : chango.getListaItems()) {// para cada item del chango
+			restarLote(it.getProductoModel() , it.getCantidad()); // // resto la cantidad de productos correspondientes a el local
+		}
+	}
+	public void sumarChango(ChangoModel chango) {
+		for ( ItemModel it : chango.getListaItems()) {// para cada item del chango
+			sumarLote(it.getProductoModel() , it.getCantidad());// sumo la cantidad de productos correspondientes a el local
+		}
+	}
 }
