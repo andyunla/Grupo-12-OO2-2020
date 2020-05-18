bb386126daf4b3d21394f0c3eadfd1c9f8905254
leanjlg <leanjlg@gmail.com>
2020-05-18 20:29:07 -0300

    findfacturas entre  fechas

--- src/main/java/com/sistema/application/repositories/IFacturaRepository.java
+++ src/main/java/com/sistema/application/repositories/IFacturaRepository.java
@@ -15,4 +15,6 @@ public interface IFacturaRepository  extends JpaRepository<Factura, Serializable
 	public abstract Factura findByIdFactura(long idFactura);
 	@Query("FROM Factura  WHERE id_local = :idLocal AND fecha_factura BETWEEN :fecha1 AND :fecha2")
 	public abstract Set<Factura> findFacturasEntreFechasLocal(LocalDate fecha1, LocalDate fecha2, long idLocal);
+	@Query("FROM Factura  WHERE  fecha_factura BETWEEN :fecha1 AND :fecha2")
+	public abstract Set<Factura> findFacturasEntreFechasLocal(LocalDate fecha1, LocalDate fecha2);
 }
