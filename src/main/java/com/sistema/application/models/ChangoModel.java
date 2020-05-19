4a08c97016161fd6fdc47687d5620fcb851eea88
Cristian Villafane <cristianv129@hotmail.com>
2020-05-19 19:28:56 -0300

    Metodos de abm de items agregados a ChangoModel

--- src/main/java/com/sistema/application/models/ChangoModel.java
+++ src/main/java/com/sistema/application/models/ChangoModel.java
@@ -1,19 +1,20 @@
 package com.sistema.application.models;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 public class ChangoModel {
-	
-	//Atributos
+
+	// Atributos
 	private long idChango;
 	private PedidoStockModel pedidoStock;
 	private LocalModel local;
 	private Set<ItemModel> listaItems;
-	
-	
-	//Constructores
-	public ChangoModel() {}
+
+	// Constructores
+	public ChangoModel() {
+	}
 
 	public ChangoModel(long idChango, PedidoStockModel pedidoStock, LocalModel local) {
 		super();
@@ -21,18 +22,19 @@ public class ChangoModel {
 		this.pedidoStock = pedidoStock;
 		this.local = local;
 	}
-	//Constructor para crerar un chango en un local
+
+	// Constructor para crerar un chango en un local
 	public ChangoModel(LocalModel local) {
-		super();		
+		super();
 		this.pedidoStock = null;
 		this.local = local;
 	}
-	
-	
-	//Getters y Setters
+
+	// Getters y Setters
 	public long getIdChango() {
 		return idChango;
 	}
+
 	public void setIdChango(long idChango) {
 		this.idChango = idChango;
 	}
@@ -40,6 +42,7 @@ public class ChangoModel {
 	public PedidoStockModel getPedidoStock() {
 		return pedidoStock;
 	}
+
 	public void setPedidoStock(PedidoStockModel pedidoStock) {
 		this.pedidoStock = pedidoStock;
 	}
@@ -47,11 +50,11 @@ public class ChangoModel {
 	public LocalModel getLocal() {
 		return local;
 	}
+
 	public void setLocal(LocalModel local) {
 		this.local = local;
 	}
 
-	
 	public Set<ItemModel> getListaItems() {
 		return listaItems;
 	}
@@ -60,7 +63,7 @@ public class ChangoModel {
 		this.listaItems = listaItems;
 	}
 
-	//toString
+	// toString
 	@Override
 	public String toString() {
 		return "ChangoModel [idChango=" + idChango + ", pedidoStock=" + pedidoStock + ", local=" + local + "]";
@@ -76,5 +79,48 @@ public class ChangoModel {
 				obj = itr.next();
 		}
 		return obj;
+	}
+
+	public ItemModel traerItem(long idItem) {
+		ItemModel obj = null;
+		Iterator<ItemModel> itr = listaItems.iterator();
+		while (obj == null && itr.hasNext()) {
+			ItemModel item = itr.next();
+			if (item.getIdItem() == idItem) {
+				obj = itr.next();
+			}
+		}
+		return obj;
+	}
+
+	public boolean crearItem(int cantidad, ProductoModel producto) {
+		// Le saqué la creación del id porque eso lo hace la base de datos
+		if (traerItem(producto) != null) {
+			traerItem(producto).setCantidad(traerItem(producto).getCantidad() + cantidad);
+		} else {
+			listaItems.add(new ItemModel(cantidad, producto, this));
+		}
+		return true;
+	}
+
+	public boolean eliminarItem(int cantidad, ProductoModel producto) throws Exception {
+		if (traerItem(producto) == null)
+			throw new Exception("El producto " + producto.getNombre() + " no existe");
+		if (traerItem(producto).getCantidad() - cantidad <= 0)
+			listaItems.remove(traerItem(producto));
+		else
+			traerItem(producto).setCantidad(traerItem(producto).getCantidad() - cantidad);
+		return true;
+	}
+
+	/*****************************************************************************************************************/
+	public double calcularTotalChango() {
+		double total = 0;
+		Iterator<ItemModel> itr = listaItems.iterator();
+		while (itr.hasNext()) {
+			ItemModel itemActual = itr.next();
+			total += itemActual.getCantidad() *itemActual.getProductoModel().getPrecio();
+		}
+		return total;
 	}
 }
