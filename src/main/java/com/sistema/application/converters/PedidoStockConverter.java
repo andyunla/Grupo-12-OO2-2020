830bab19671eada3a35e55063e2a8d2bced6b8e8
Cristian Villafane <cristianv129@hotmail.com>
2020-05-20 02:35:16 -0300

    Arreglada excepcion por oferente nulo en PedidoStockConverter

--- src/main/java/com/sistema/application/converters/PedidoStockConverter.java
+++ src/main/java/com/sistema/application/converters/PedidoStockConverter.java
@@ -1,11 +1,13 @@
 package com.sistema.application.converters;
 
+import com.sistema.application.models.EmpleadoModel;
 import com.sistema.application.models.PedidoStockModel;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import com.sistema.application.entities.Empleado;
 import com.sistema.application.entities.PedidoStock;
 
 @Component("pedidoStockConverter")
@@ -16,14 +18,26 @@ public class PedidoStockConverter {
 	@Autowired
 	@Qualifier("empleadoConverter")
 	private EmpleadoConverter empleadoConverter;
-	
+
 	public PedidoStockModel entityToModel(PedidoStock pedidoStock) {
-		return new PedidoStockModel(pedidoStock.getIdPedidoStock(), productoConverter.entityToModel(pedidoStock.getProducto()), 
-				pedidoStock.getCantidad(), pedidoStock.isAceptado(), empleadoConverter.entityToModel(pedidoStock.getEmpleadoSolicitante()), empleadoConverter.entityToModel(pedidoStock.getEmpleadoOferente()));
+		// Verifica si el oferente es nulo, de serlo entonces no realiza su conversion a modelo
+		Empleado oferente = pedidoStock.getEmpleadoOferente();
+		EmpleadoModel oferenteModel = (oferente == null) ? null : empleadoConverter.entityToModel(oferente);
+		return new PedidoStockModel(pedidoStock.getIdPedidoStock(),
+				productoConverter.entityToModel(pedidoStock.getProducto()), pedidoStock.getCantidad(),
+				pedidoStock.isAceptado(), empleadoConverter.entityToModel(pedidoStock.getEmpleadoSolicitante()),
+				oferenteModel);
 	}
-	
-	public PedidoStock modelToEntity(PedidoStockModel pedidoStockModel) {		
-		return new PedidoStock(pedidoStockModel.getIdPedidoStock(), productoConverter.modelToEntity(pedidoStockModel.getProducto()), 
-				pedidoStockModel.getCantidad(), pedidoStockModel.isAceptado(), empleadoConverter.modelToEntity(pedidoStockModel.getEmpleadoSolicitante()), empleadoConverter.modelToEntity(pedidoStockModel.getEmpleadoOferente()));
+
+	public PedidoStock modelToEntity(PedidoStockModel pedidoStockModel) {
+		// Verifica si el oferente es nulo, de serlo entonces no realiza su conversion a modelo
+		EmpleadoModel oferente = pedidoStockModel.getEmpleadoOferente();
+		Empleado oferenteModel = (oferente == null) ? null : empleadoConverter.modelToEntity(oferente);
+
+		return new PedidoStock(pedidoStockModel.getIdPedidoStock(),
+				productoConverter.modelToEntity(pedidoStockModel.getProducto()), pedidoStockModel.getCantidad(),
+				pedidoStockModel.isAceptado(),
+				empleadoConverter.modelToEntity(pedidoStockModel.getEmpleadoSolicitante()),
+				oferenteModel);
 	}
 }
