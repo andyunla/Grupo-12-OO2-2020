ef35f0890bcccb96c09660a5511c5181b7e86dd5
Cristian Villafane <cristianv129@hotmail.com>
2020-05-22 17:58:05 -0300

    metodos innecesarios eliminados de rest controller

--- src/main/java/com/sistema/application/controllers/api/v1/LocalRestController.java
+++ src/main/java/com/sistema/application/controllers/api/v1/LocalRestController.java
@@ -5,10 +5,7 @@ import java.util.List;
 
 import com.sistema.application.converters.EmpleadoConverter;
 import com.sistema.application.models.EmpleadoModel;
-import com.sistema.application.models.LocalModel;
-import com.sistema.application.models.ProductoModel;
 import com.sistema.application.models.dto.EmpleadoDto;
-import com.sistema.application.models.dto.LocalDistanciaDto;
 import com.sistema.application.services.IEmpleadoService;
 import com.sistema.application.services.ILocalService;
 import com.sistema.application.services.IProductoService;
@@ -38,17 +35,6 @@ public class LocalRestController {
     @Qualifier("productoService")
     private IProductoService productoService;
     
-    
-    // Faltaría implementar en caso de errores
-    @GetMapping("distancia/{idLocal1}/{idLocal2}")
-    ResponseEntity <Double> distancia(@PathVariable("idLocal1") long idLocal1,
-                                      @PathVariable("idLocal2") long idLocal2) {
-        LocalModel local1 = localService.findByIdLocal(idLocal1);
-        LocalModel local2 = localService.findByIdLocal(idLocal2);
-        double distancia = local1.calcularDistancia(local2);
-        return new ResponseEntity <Double>(distancia, HttpStatus.OK);
-    }
-    
     @GetMapping("empleados/{idLocal}")
     ResponseEntity <List<EmpleadoDto>> empleados(@PathVariable("idLocal") long idLocal) {
         List<EmpleadoModel> models = empleadoService.findByIdLocal(idLocal);
@@ -58,23 +44,5 @@ public class LocalRestController {
             empleados.add(empleado);
         }
         return new ResponseEntity<List<EmpleadoDto>>(empleados, HttpStatus.OK);
-    }
-
-    // API REST que retorna los 3 locales más cercano dado el 
-    // producto elegido y su cantidad
-    @GetMapping("distancia/{idLocal}/{idProducto/{cantidad}/")
-    ResponseEntity <List<LocalDistanciaDto>> localesMasCercanos(@PathVariable("idLocal") long idLocal,
-                                                                @PathVariable("idProducto") long idProducto,
-                                                                @PathVariable("cantidad") int cantidad) {
-        LocalModel local = localService.findByIdLocal(idLocal);
-        ProductoModel producto = productoService.findByIdProducto(idProducto);
-        List<LocalModel> listaLocales = local.localesCercanos(producto, cantidad);
-        List<LocalDistanciaDto> localesCercanos = new ArrayList<LocalDistanciaDto>();
-        for(LocalModel model: listaLocales) {
-            LocalDistanciaDto dto = new LocalDistanciaDto(model.getIdLocal(), model.getNombreLocal(), local.calcularDistancia(model),
-                                    model.getDireccion(), model.getTelefono());
-            localesCercanos.add(dto);
-        }
-        return new ResponseEntity<List<LocalDistanciaDto>>(localesCercanos, HttpStatus.OK);
     }
 }
