79d77740276332aa3bb3de724b36c50c45210bb1
andyunla <andy.dev.alnu@hotmail.com>
2020-05-22 21:46:53 -0300

    Update SecurityConfiguration

--- src/main/java/com/sistema/application/configuration/SecurityConfiguration.java
+++ src/main/java/com/sistema/application/configuration/SecurityConfiguration.java
@@ -1,18 +1,69 @@
 package com.sistema.application.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.sistema.application.services.implementations.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    /*
     @Override
     protected void configure(HttpSecurity http) throws Exception {
          http.authorizeRequests()
          .anyRequest().authenticated()
          .and().formLogin();    // Configuración para que el login por defecto siga
          http.csrf().disable(); // Configuración para permitir llamadas AJAX sin tokens
+    }
+    */
+    
+    String[] resources = new String[]{
+            "/include/**","/css/**","/icons/**","/img/**","/js/**","/layer/**"
+    };
+	
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	http.authorizeRequests()
+        .antMatchers(resources).permitAll()  
+        .antMatchers("/","/index","/signup").permitAll()
+            .anyRequest().authenticated()
+            .and()
+        .formLogin()
+            .loginPage("/login")
+            .permitAll()
+            .defaultSuccessUrl("/userForm")
+            .failureUrl("/login?error=true")
+            .usernameParameter("username")
+            .passwordParameter("password")
+            .and()
+            .csrf().disable()
+        .logout()
+            .permitAll()
+            .logoutSuccessUrl("/login?logout");
+    }
+	
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+		bCryptPasswordEncoder = new BCryptPasswordEncoder(4);
+        return bCryptPasswordEncoder;
+    }
+    
+    @Autowired
+    UserDetailsService userDetailsService;
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception { 
+    	//Especificar el encargado del login y encriptacion del password
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 }
