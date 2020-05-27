48bd4a6e43fb17f8102e6122db97d904a69d03b0
andyunla <andy.dev.alnu@hotmail.com>
2020-05-26 22:32:21 -0300

    Agregado UtilHelper

---
+++ src/main/java/com/sistema/application/helpers/UtilHelper.java
@@ -0,0 +1,7 @@
+package com.sistema.application.helpers;
+
+public class UtilHelper {
+	/**** ROLES *********************************************/
+	public final static String ROLE_EMPLEADO = "EMPLEADO";
+	public final static String ROLE_GERENTE = "GERENTE";
+}
