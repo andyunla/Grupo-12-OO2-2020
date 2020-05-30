1437a83b12a46aa0aca8f0ac42a5606a302555f3
leanjlg <leanjlg@gmail.com>
2020-05-29 23:00:58 -0300

    ProductoConverter modelToDto

--- src/main/java/com/sistema/application/converters/ProductoConverter.java
+++ src/main/java/com/sistema/application/converters/ProductoConverter.java
@@ -1,5 +1,6 @@
 package com.sistema.application.converters;
 
+import com.sistema.application.dto.ProductoRankingDto;
 import com.sistema.application.entities.Producto;
 import com.sistema.application.models.ProductoModel;
 
@@ -17,4 +18,11 @@ public class ProductoConverter {
 		return new Producto(producto.getIdProducto(), producto.getNombre(), producto.getDescripcion(),
 				   producto.getPrecio(), producto.getTalle());
 	}
+	// models to DTO
+	// *******************************************************
+	public ProductoRankingDto modelToDto (ProductoModel pro, int cantidad) {
+		return new ProductoRankingDto(pro.getIdProducto(), pro.getNombre(), pro.getDescripcion(), pro.getPrecio(), 
+				pro.getTalle(), cantidad);
+	}
+	
 }
