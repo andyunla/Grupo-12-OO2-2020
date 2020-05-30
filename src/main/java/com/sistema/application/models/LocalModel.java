b1d621a6605100a53877b7bd04c4cf6bc581bfe9
Franco2900 <franco2900@gmail.com>
2020-05-30 12:42:19 -0300

    Más datos en la Base de Datos y fusion de Funciones y Funciones1

--- src/main/java/com/sistema/application/models/LocalModel.java
+++ src/main/java/com/sistema/application/models/LocalModel.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.sistema.application.converters.ProductoConverter;
-import com.sistema.application.funciones.Funciones1;
+import com.sistema.application.funciones.Funciones;
 import com.sistema.application.services.IChangoService;
 import com.sistema.application.services.IFacturaService;
 import com.sistema.application.services.ILocalService;
@@ -240,7 +240,7 @@ public class LocalModel {
 			id[i] = listaLocales.get(i).getIdLocal();
 			distancia[i] = calcularDistancia(listaLocales.get(i));
 		}
-		Funciones1.orden(id, distancia);// este método ordena los dos vectores de mayor a menor usando el valor de la
+		Funciones.orden(id, distancia);// este método ordena los dos vectores de mayor a menor usando el valor de la
 										// distancia
 		for (long l : id) {
 			lista.add(localService.findByIdLocal(l));//agrego a la lista los productos con ID en orden
@@ -286,7 +286,7 @@ public class LocalModel {
 	}
 
 	public boolean restarLote(ProductoModel producto, int cantidad) {
-		int i =0;
+
 		//traigo la lista de lotes del producto en el local
 		Set<LoteModel> lista = loteService.findByLoteProductoActivo(producto.getIdProducto(), this.idLocal);
 		Iterator<LoteModel> itr = lista.iterator();
@@ -304,13 +304,13 @@ public class LocalModel {
 				loteService.insertOrUpdate(lo);// actualizo el lote en la base de datos
 				cantidad =0;// seteo en cero para salir del bucle, ya no hay mas que restar
 			}			
-		i++;	
+
 		}		
 		return true;
 	}
 
 	public boolean sumarLote(ProductoModel producto, int cantidad) {
-		int i =0;
+		
 		//traigo la lista de lotes del producto en el local
 		Set<LoteModel> lista = loteService.findByLoteProductoBaja(producto.getIdProducto(), this.idLocal);
 		Iterator<LoteModel> itr = lista.iterator();
@@ -329,7 +329,7 @@ public class LocalModel {
 				loteService.insertOrUpdate(lo);// actualizo el lote en la base de datos
 				cantidad =0;// seteo en cero para salir del bucle, ya no hay mas que sumar
 			}			
-		i++;	
+	
 		}		
 		return true;
 	}
@@ -524,7 +524,7 @@ public class LocalModel {
 			idList[i] = listaProductos.get(i).getIdProducto();
 			cantidadList[i] = cantidadProductoVendido(listaProductos.get(i));
 		}
-		Funciones1.orden(idList, cantidadList);// este método ordena los dos vectores de mayor a menor usando el valor de la
+		Funciones.orden(idList, cantidadList);// este método ordena los dos vectores de mayor a menor usando el valor de la
 									   // cantidad
 		int cont = 0;
 		for (long id : idList) {
