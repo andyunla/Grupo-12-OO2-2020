c70384ded698a6696d5acbde89ce5b2d0f1b1b5f
Cristian Villafane <cristianv129@hotmail.com>
2020-05-30 22:34:28 -0300

    arreglado error al cancelar un chango

--- src/main/java/com/sistema/application/controllers/ChangoController.java
+++ src/main/java/com/sistema/application/controllers/ChangoController.java
@@ -42,15 +42,15 @@ public class ChangoController {
      @Autowired
      @Qualifier("userRepository")
      private IUserRepository userRepository;
-     
+
      @Autowired
      @Qualifier("productoService")
      private IProductoService productoService;
-     
+
      @Autowired
      @Qualifier("localConverter")
      private LocalConverter localConverter;
-     
+
      @Autowired
      @Qualifier("userConverter")
      private UserConverter userConverter;
@@ -70,7 +70,7 @@ public class ChangoController {
      @Autowired
      @Qualifier("productoConverter")
      private ProductoConverter productoConverter;
-     
+
      @GetMapping("")
      public String chango(Model modelo) {
           // Obtengo el usuario de la sesión
@@ -80,14 +80,15 @@ public class ChangoController {
           userDto.setTipoGerente(isGerente);
           modelo.addAttribute("currentUser", userDto);
           // Obtenemos el local actual donde trabaja el usuario
-          LocalModel localActual = this.getLocalGivenUser(user.getUsername());
+          LocalModel localActual = this.getLocalGivenUser(userDto.getUsername());
           localModel.setInstance(localActual);
-          // Selecciono de todos los productos solo los que tienen por lo menos 1 en stock en el local
+          // Selecciono de todos los productos solo los que tienen por lo menos 1 en stock
+          // en el local
           List<ProductoStockDto> productosConStock = new ArrayList<ProductoStockDto>();
           for (ProductoModel p : productoService.getAllModel()) {
                int stock = localModel.calcularStockLocal(p);
-               if ( stock > 0) {
-                    productosConStock.add( productoConverter.modelToDTO(p, stock) );
+               if (stock > 0) {
+                    productosConStock.add(productoConverter.modelToDTO(p, stock));
                }
           }
           ChangoModel nuevoChango = localModel.crearChango();
@@ -95,29 +96,31 @@ public class ChangoController {
           modelo.addAttribute("chango", nuevoChango);
           return ViewRouteHelper.CHANGO_ROOT;
      }
-         
+
      // Agrega un nuevo item al chango, su cantidad será 1
      @PostMapping("nuevo-item/{idChango}/{idProducto}")
-     public ModelAndView agregarItem(@PathVariable("idChango") long idChango, @PathVariable("idProducto") long idProducto) {
-          //localModel.setInstance(localService.findByIdLocal(1));
+     public ModelAndView agregarItem(@PathVariable("idChango") long idChango,
+               @PathVariable("idProducto") long idProducto) {
+          // localModel.setInstance(localService.findByIdLocal(1));
           ModelAndView mAV = new ModelAndView(ViewRouteHelper.ITEM);
           // Verifico que el producto elegido no esté ya en un item del chango
           if (itemService.findByChangoAndProducto(idChango, idProducto) == null) {
-               ItemModel newItem = new ItemModel(1, productoService.findByIdProducto(idProducto), changoService.findByIdChango(idChango));
+               ItemModel newItem = new ItemModel(1, productoService.findByIdProducto(idProducto),
+                         changoService.findByIdChango(idChango));
                ItemModel item = itemService.insertOrUpdate(newItem);
                mAV.addObject("item", item);
-               mAV.setStatus(HttpStatus.CREATED); 
+               mAV.setStatus(HttpStatus.CREATED);
                localModel.restarLote(item.getProductoModel(), 1);
           } else {
                mAV.setStatus(HttpStatus.NOT_ACCEPTABLE);
           }
           return mAV;
-     }     
+     }
 
      // Elimina un item de un chango abierto
      @PostMapping("eliminar-item/{idItem}")
      public ResponseEntity<String> eliminarItem(@PathVariable("idItem") long idItem) {
-          //localModel.setInstance(localService.findByIdLocal(1));
+          // localModel.setInstance(localService.findByIdLocal(1));
           ItemModel item = itemService.findByIdItem(idItem);
           ProductoModel producto = item.getProductoModel();
           if (itemService.remove(idItem)) {
@@ -126,45 +129,46 @@ public class ChangoController {
           } else {
                return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
           }
-     }              
+     }
 
      // Cambia la cantidad de un item
      @PostMapping("modificar-item/{idItem}/{nuevaCantidad}")
      public ResponseEntity<String> modificar(@PathVariable("idItem") long idItem,
                @PathVariable("nuevaCantidad") int nuevaCantidad) {
           ItemModel item = itemService.findByIdItem(idItem);
-          //localModel.setInstance(localService.findByIdLocal(1));
-          // Verifico si estoy agregando cantidad al item   
+          // localModel.setInstance(localService.findByIdLocal(1));
+          // Verifico si estoy agregando cantidad al item
           if (item.getCantidad() < nuevaCantidad) {
                // Verifico si hay stock en el local
                if (localModel.validarStock(item.getProductoModel(), nuevaCantidad - item.getCantidad())) {
                     // Resto a los lotes del local la diferencia entre la nueva cantidad que se sumó
                     localModel.restarLote(item.getProductoModel(), nuevaCantidad - item.getCantidad());
                } else {
-                    // No se realizan cambios y devuelvo el una respuesta con la cantidad actual del item
+                    // No se realizan cambios y devuelvo el una respuesta con la cantidad actual del
+                    // item
                     return ResponseEntity.badRequest().body(String.valueOf(item.getCantidad()));
-               }      
-          } else { 
+               }
+          } else {
                // Sumo a los lotes del local la diferencia entre la nueva cantidad que se restó
                localModel.devolverLote(item.getProductoModel(), item.getCantidad() - nuevaCantidad);
           }
           item.setCantidad(nuevaCantidad);
           itemService.insertOrUpdate(item);
-          return new ResponseEntity<String>(HttpStatus.OK); 
+          return new ResponseEntity<String>(HttpStatus.OK);
      }
 
      @PostMapping("eliminar/{idChango}")
      public String eliminarChango(@PathVariable("idChango") long idChango) {
           // Traer items del chango, devolverlos a sus lotes y eliminarlos
-          //localModel.setInstance(localService.findByIdLocal(1));
+          // localModel.setInstance(localService.findByIdLocal(1));
           List<ItemModel> items = itemService.findByChango(idChango);
-          for(ItemModel item: items) {
+          for (ItemModel item : items) {
                localModel.devolverLote(item.getProductoModel(), item.getCantidad());
                itemService.remove(item.getIdItem());
           }
           // Eliminar chango
           changoService.remove(idChango);
-          return ViewRouteHelper.HOME_ROOT;
+          return "redirect:/" + ViewRouteHelper.HOME_ROOT;
      }
      /*
       * CHANGO ABIERTO: Vista para permitir retomar un chango creado pero aun sin
@@ -178,11 +182,12 @@ public class ChangoController {
      // TODO Buscar donde se auto eliminarán aquellos changos guardados sin items
 
      /**
-     * Método que retorna el local donde trabaja el usuario
-     * que está logueado actualmente dado su username
-     * @param username Tipo String. Ej: 'empleado1'
-     * @return LocalModel
-     */
+      * Método que retorna el local donde trabaja el usuario que está logueado
+      * actualmente dado su username
+      * 
+      * @param username Tipo String. Ej: 'empleado1'
+      * @return LocalModel
+      */
      private LocalModel getLocalGivenUser(String username) {
           com.sistema.application.entities.User user = userRepository.findByUsernameAndFetchUserRolesEagerly(username);
           Local local = user.getEmpleado().getLocal();
