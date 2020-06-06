5a4afaca697aef5ee290b004fc1e050fa1b08359
Cristian Villafane <cristianv129@hotmail.com>
2020-06-06 04:31:09 -0300

    Agregada descarga de factura

--- src/main/resources/static/js/factura.js
+++ src/main/resources/static/js/factura.js
@@ -11,3 +11,24 @@ JsBarcode(".codigo")
           fontSize: 10
      })
      .init();
+
+function descargar() {
+     var element = document.getElementById('container-container');
+     var opt = {
+          margin: 1,
+          filename: 'factura.pdf',
+          enableLinks: true,
+          image: { type: 'jpeg', quality: 0.99 },
+          html2canvas: {
+               scale: 2,
+               allowTaint: true
+          },
+          jsPDF: {
+               orientation: 'p',
+               unit: 'mm',
+               format: [220, 311],
+               putOnlyUsedFonts: true
+          }
+     };
+     html2pdf().from(element).set(opt).save();
+}
