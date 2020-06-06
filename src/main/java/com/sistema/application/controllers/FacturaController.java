afededcb5324d651c6a157ee107c2ece92ac1c1f
Cristian Villafane <cristianv129@hotmail.com>
2020-06-06 01:56:07 -0300

    Implementada factura tras confirmacion de carrito

--- src/main/java/com/sistema/application/controllers/FacturaController.java
+++ src/main/java/com/sistema/application/controllers/FacturaController.java
@@ -1,6 +1,7 @@
 package com.sistema.application.controllers;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import com.sistema.application.converters.LocalConverter;
 import com.sistema.application.converters.UserConverter;
@@ -8,10 +9,12 @@ import com.sistema.application.dto.UserDto;
 import com.sistema.application.models.ChangoModel;
 import com.sistema.application.models.ClienteModel;
 import com.sistema.application.models.FacturaModel;
+import com.sistema.application.models.ItemModel;
 import com.sistema.application.services.IChangoService;
 import com.sistema.application.services.IClienteService;
 import com.sistema.application.services.IEmpleadoService;
 import com.sistema.application.services.IFacturaService;
+import com.sistema.application.services.IItemService;
 import com.sistema.application.services.ILocalService;
 import com.sistema.application.services.implementations.UserService;
 import com.sistema.application.helpers.ViewRouteHelper;
@@ -65,16 +68,18 @@ public class FacturaController {
      @Autowired
      @Qualifier("localService")
      private ILocalService localService;
+
+     @Autowired
+     @Qualifier("itemService")
+     private IItemService itemService;
      
      @PostMapping("confirmar/{idChango}")
      public ModelAndView crearFactura(@ModelAttribute ClienteModel cliente, 
           @PathVariable("idChango") long idChango ) 
      {
-          // TODO: Redireccionar hacia la factura creada
-          ModelAndView mAV = new ModelAndView("redirect:/chango/todos");
+          ModelAndView mAV = new ModelAndView();
           UserDto userDto = userService.getCurrentUser();
           mAV.addObject("currentUser", userDto); 
-          // Obtiene local
           FacturaModel nuevaFactura = new FacturaModel(   
                clienteService.findByNroCliente(cliente.getNroCliente()),
                changoService.findByIdChango(idChango),
@@ -84,15 +89,22 @@ public class FacturaController {
                localService.findByIdLocal(userDto.getIdLocal())
                );
           FacturaModel facturaGuradada = facturaService.insertOrUpdate(nuevaFactura);
+          mAV.setViewName("redirect:/factura/ver/" + facturaGuradada.getIdFactura());
           changoSesion.clear();
           return mAV; 
      }
 
      @GetMapping("ver/{idFactura}")
-     public ModelAndView traerFactura(@PathVariable ("idFactura") long id) {
-          UserDto userDto = userService.getCurrentUser();
+     public ModelAndView traerFactura(@PathVariable ("idFactura") long idFactura) {
+          //TODO: Controlar el caso en que se necesiten dos hojas
+          //TODO: Generar codigo de barra
           ModelAndView mAV = new ModelAndView(ViewRouteHelper.FACTURA);
+          FacturaModel factura = facturaService.findByIdFactura(idFactura);
+          UserDto userDto = userService.getCurrentUser();
+          List <ItemModel> items = itemService.findByChango(factura.getChango().getIdChango());
           mAV.addObject("currentUser", userDto); 
+          mAV.addObject("factura", factura);
+          mAV.addObject("items", items);
           return mAV;
      }
 }
