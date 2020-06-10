dea59e56fcb42ea464963de82884371579ef1a83
andyunla <andy.dev.alnu@hotmail.com>
2020-06-10 14:07:24 -0300

    script js ahora envía objeto de respuesta completo

--- src/main/resources/static/js/client.js
+++ src/main/resources/static/js/client.js
@@ -1,7 +1,7 @@
 const TIME_LOOP = 4000; // Cada 4s
 const host = "http://localhost:8080";
-const url_api = host + "/api/v1/notificacion"
-const url_pedido = host + "/pedido"
+const url_api = host + "/api/v1/notificacion";
+const url_pedido = host + "/pedido";
 
 /* Comprobar si hay nuevas notificaciones para el usuario */
 function comprobar() {
@@ -37,15 +37,19 @@ function comprobarRespuestas() {
         });
 }
 
-// En caso que se acepte o no la solicitud
-function realizarSolicitudPedido(detalleEnvio) {
+// 1ro se realiza el pedido y obtenemos el id en caso que se haya realizado con éxito.
+// Luego se envía la respuesta
+async function realizarSolicitudPedido(detalleEnvio) {
     // Se construye un Objeto msg que contiene los datos para enviar en la respuesta; no para realizar el pedido
-    var msg = {
-        idNotificacion: detalleEnvio.idNotificacion,
-        aceptado: detalleEnvio.aceptado? true : false,
+    var respuesta = {
+        id: detalleEnvio.idNotificacion,
+        tipo: "RESPUESTA",
         texto: detalleEnvio.aceptado? "ACEPTADO" : "RECHAZADO",
         from: document.getElementById("current-username").value, // El username del usuario actual
-        to: detalleEnvio.userSolicitante // El dueño que envío la solicitud; le devolvemos la respuesta
+        to: detalleEnvio.userSolicitante, // El dueño que envío la solicitud; le devolvemos la respuesta
+        detalleNotificacion: {
+            idPedidoStock: null
+        }
     };
     // Se realiza la factura
     let userOferente = detalleEnvio.userOferente;
@@ -54,38 +58,43 @@ function realizarSolicitudPedido(detalleEnvio) {
     let idProducto = detalleEnvio.idProducto;
     let cantidad = detalleEnvio.cantidad;
     let urlSolicitud = url_pedido + "/solicitar/" + userOferente + "/" + userSolicitante + "/" + fueAceptado + "/" + idProducto + "/" + cantidad;
-    fetch(urlSolicitud, { method: 'POST' })
-        .then(res => {
-            if (res.status == 200 || res.status == 201) {
-                //listarMasCercanos(); // Refrescamos la lista de locales cercanos
-                console.log("Solicitud realizada");
-            } else {
-                // Si ocurre un error
-                msg.texto = "ERROR: HUBO UN PROBLEMA EN LA TRANSACCIÓN"
-            }
-        })
-        .catch(e => { console.error(e) });
+    try {
+        let response = await fetch(urlSolicitud, { method: 'POST' });
+        if (response.ok) { // 200 - 299
+            let text = await response.text();
+            console.log("Solicitud realizada");
+            respuesta.detalleNotificacion.idPedidoStock = JSON.parse(text).idPedidoStock; // El id del pedido realizado
+        } else {
+            throw new Error();
+        }
+    } catch(e) {
+        console.error(e);
+    }
     // Enviamos la respuesta
-    enviarRespuesta(msg);
+    enviarRespuesta(respuesta);
 }
 
-function enviarRespuesta(msg) {
-    let url = url_api + "/responder/" + msg.from + "/" + msg.to + "/" + msg.aceptado;
-    fetch(url, { method: 'GET' })
-        .then(res => {
-            if (res.status == 200 || res.status == 201) {
-                //listarMasCercanos(); // Refrescamos la lista de locales cercanos
-                console.log("Respuesta enviada");
-            } else {
-                // Si ocurre un error
-                msg.texto = "ERROR: HUBO UN PROBLEMA EN EL ENVÍO DE LA RESPUESTA"
-            }
-        })
-        .catch(e => { console.error(e) });
+function enviarRespuesta(respuesta) {
+    $.ajax({
+        type : "POST",
+        url : url_api + "/responder",
+        dataType: "json",
+        headers: { 
+            'Accept': 'application/json',
+            'Content-Type': 'application/json' 
+        },
+        data : JSON.stringify(respuesta),
+        success : function(response) {
+           console.log('Respuesta enviada');
+        },
+        error : function(e) {
+           console.error("ERROR: " + e.message);   
+        }
+    });
     // Le mandamos el id para que lo marque como leída
-    confirmarRespuestas(msg.idNotificacion);
+    confirmarRespuestas(respuesta.id);
     // Eliminamos la notificación de la lista
-    document.getElementById("notificacion-" + msg.idNotificacion).remove();
+    document.getElementById("notificacion-" + respuesta.id).remove();
 }
 
 // Añade los listener a los botones para aceptar el pedido
@@ -129,17 +138,9 @@ function cargarNotificaciones(lista) {
         document.getElementById("notificationContainer").innerHTML = "";
         for (i = 0; i < size; i++) {
             var obj = lista[i];
-            if(obj.tipo.toUpperCase() === "solicitud".toUpperCase()) { // Solicitud
-                let idLocal = document.getElementById('current-idLocal').value;
-                if(obj.toLocal == idLocal) { // Si la solicitud es para este local
-                    var html = renderizarAHTML(obj);
-                    var htmlAnterior = document.getElementById("notificationContainer").innerHTML
-                    document.getElementById("notificationContainer").innerHTML = html + htmlAnterior;
-                }
-            } else { // Si lo que me llega es una respuesta
-                if(obj.to == document.getElementById("current-username").value) // Si son mensajes para el usuario actual
-                    alertarRespuesta(obj);
-            }
+            var html = renderizarAHTML(obj);
+            var htmlAnterior = document.getElementById("notificationContainer").innerHTML
+            document.getElementById("notificationContainer").innerHTML = html + htmlAnterior;
         }
         agregarListenerABotonesAceptar();
         agregarListenerABotonesRechazar();
@@ -148,43 +149,54 @@ function cargarNotificaciones(lista) {
 }
 
 function renderizarAHTML(obj) {
-    var msg = "El usuario <strong>" + obj.from + "</strong> necesita " + obj.detalleNotificacion.cantidad + " unidad(es) de " + 
-              '<strong>' + obj.detalleNotificacion.nombreProducto + '</strong>';
-    var html = '<div class="dropdown-item" id="notificacion-' + obj.id + '">' + 
-                '<div class="alert alert-info">' +
-                    msg +
-                    '<div class="alert alert-info">' +
-                        '<a href="#" class="btn btn-xs btn-primary pull-right botonAceptar" data-id="' + obj.id + 
-                            '" data-user-from="' + obj.from + '" data-id-producto="' + obj.detalleNotificacion.idProducto + 
-                            '" data-cantidad="' + obj.detalleNotificacion.cantidad + '"> Aceptar</a>' +
-                        '<a href="#" class="btn btn-xs btn-primary pull-right botonRechazar" data-id="' + obj.id + 
-                            '" data-user-from="' + obj.from + '" data-id-producto="' + obj.detalleNotificacion.idProducto + 
-                            '" data-cantidad="' + obj.detalleNotificacion.cantidad + '"> Rechazar</a>' +
-                    '</div>' +
-                '<div class="dropdown-divider"></div>' +
-           '</div>';
-    return html;
-}
-
-// Inserta el nodo alert en el DOM
-function alertarRespuesta(obj) {
-    let alertCloseButton = '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
-                           '<span aria-hidden="true">&times;</span></button>';
-    let htmlAlert = "";
-    switch(obj.texto.toUpperCase()) {
-      case "ACEPTADO":
-        htmlAlert = '<div data-id-notificacion="' + obj.id + '" class="p-4 alert alert-success alert-dismissible fade show" role="alert">' +
-                    '<strong>EXITO</strong> El pedido ha sido aceptado' + alertCloseButton + '</div> ';
-        break;
-      case "RECHAZADO":
-        htmlAlert = '<div data-id-notificacion="' + obj.id + '" class="p-4 alert alert-danger alert-dismissible fade show" role="alert">' +
-                    '<strong>ERROR</strong> El pedido ha sido rechazado' + alertCloseButton + '</div> ';
-        break;
-      default: // Algún error
-        htmlAlert = '<div data-id-notificacion="' + obj.id + '" class="p-4 alert alert-danger alert-dismissible fade show" role="alert">' +
-                    '<strong>' + obj.texto + '</strong>' + alertCloseButton + '</div> ';
+    var cargado = false;
+    if(obj.tipo.toUpperCase() === "SOLICITUD") { // Solicitud
+        var msg = "El usuario <strong>" + obj.from + "</strong> necesita " + obj.detalleNotificacion.cantidad + " unidad(es) de " + 
+                  "<strong>" + obj.detalleNotificacion.nombreProducto + "</strong>";
+        var botones = '<a href="#" class="btn btn-xs btn-primary pull-right botonAceptar" data-id="' + obj.id + 
+                           '" data-user-from="' + obj.from + '" data-id-producto="' + obj.detalleNotificacion.idProducto + 
+                           '" data-cantidad="' + obj.detalleNotificacion.cantidad + '"> Aceptar</a>' +
+                           '<a href="#" class="btn btn-xs btn-primary pull-right botonRechazar" data-id="' + obj.id + 
+                           '" data-user-from="' + obj.from + '" data-id-producto="' + obj.detalleNotificacion.idProducto + 
+                           '" data-cantidad="' + obj.detalleNotificacion.cantidad + '"> Rechazar</a>';
+        cargado = true;
+    } else { // Si lo que me llega es una respuesta
+        if(obj.to == document.getElementById("current-username").value) { // Si son mensajes para el usuario actual
+            switch(obj.texto.toUpperCase()) {
+                case "ACEPTADO":
+                    var msg = "<strong> El pedido ha sido aceptado </strong>";
+                    break;
+                case "RECHAZADO":
+                    var msg = "<strong> El pedido ha sido rechazado </strong>";
+                    break;
+                default:
+                    var msg = "<strong>" + obj.texto + "</strong>";
+                    break;
+            }
+            let botonFacturar = '<a href="#" class="btn btn-xs btn-primary pull-right botonFacturar" data-id="' + obj.id + 
+                                '" data-user-from="' + obj.from + '" data-id-producto="' + obj.detalleNotificacion.idProducto + 
+                                '" data-cantidad="' + obj.detalleNotificacion.cantidad + '"> Facturar</a>';
+            // En el caso de rechazo o error
+            let botonCerrar = '<a href="#" class="btn btn-xs btn-primary pull-right botonCerrar" data-id="' + obj.id + 
+                                '" data-user-from="' + obj.from + '" data-id-producto="' + obj.detalleNotificacion.idProducto + 
+                                '" data-cantidad="' + obj.detalleNotificacion.cantidad + '"> Cerrar</a>';
+            var botones = (obj.texto.toUpperCase() === "ACEPTADO")? botonFacturar : botonCerrar;
+            cargado = true; // Para no enviar html cuando los botones no se establecieron
+        }
     }
-    document.getElementById("alertRespuesta").innerHTML = htmlAlert;
+    if(cargado) {
+        var html = '<div class="dropdown-item" id="notificacion-' + obj.id + '">' + 
+                        '<div class="alert alert-info">' +
+                            msg +
+                            '<div class="alert alert-info">' +
+                                botones +
+                            '</div>' +
+                        '<div class="dropdown-divider"></div>' +
+                   '</div>';
+    } else {
+        var html = "";
+    }
+    return html;
 }
 
 function confirmarRespuestas(idNotificacion) {
