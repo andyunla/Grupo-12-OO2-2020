f86bc1e4b33d226b5aa2a6b99689caedaf55eb4d
andyunla <andy.dev.alnu@hotmail.com>
2020-06-15 15:02:29 -0300

    Agregado parche para ingresar fechas correctas

--- src/main/resources/static/js/sueldo.js
+++ src/main/resources/static/js/sueldo.js
@@ -1,19 +1,25 @@
 let fechaSeleccionada = document.getElementById("fechaSueldo");
+const MAX_YEAR = 2020;
 
 function listarMasCercanos() {
     if (fechaSeleccionada.value !== "") {
         let fecha = fechaSeleccionada.value; // Del tipo aaaa-mm
         let patt1 = /^(\d{1,4})\-(\d{1,2})$/; // Patrón
         if(fecha.match(patt1) !== null) { // Si se ingresaron los datos correctamente
-            let url = host + "/sueldos/";
-            fetch(url + "traer/" + fecha)
-                .then(response => response.text())
-                .then(html => {
-                    document.querySelector("tbody").innerHTML = html;
-                })
-                .catch((e) => {
-                    console.log(e);
-                });
+            let parts = fecha.split("-");
+            if(parts[1] <= 12 && parts[0] <= MAX_YEAR) {
+                let url = host + "/sueldos/";
+                fetch(url + "traer/" + fecha)
+                    .then(response => response.text())
+                    .then(html => {
+                        document.querySelector("tbody").innerHTML = html;
+                    })
+                    .catch((e) => {
+                        console.log(e);
+                    });
+            } else {
+                document.querySelector("tbody").innerHTML = "";
+            }
         }
     }
 }
