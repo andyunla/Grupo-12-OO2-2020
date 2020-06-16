db92da80f8ba4bd2046186a7f611b3c8b99b8814
Cristian Villafane <cristianv129@hotmail.com>
2020-06-16 20:13:53 -0300

    Modificada vista de lotes

--- src/main/java/com/sistema/application/repositories/ILoteRepository.java
+++ src/main/java/com/sistema/application/repositories/ILoteRepository.java
@@ -5,9 +5,10 @@ import java.util.List;
 import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query; 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.sistema.application.entities.Local;
 import com.sistema.application.entities.Lote;
 
 @Repository("loteRepository")
@@ -18,12 +19,16 @@ public interface ILoteRepository extends JpaRepository<Lote, Serializable> {
 	@Query("FROM Lote  WHERE id_local = (:idLocal) AND id_producto=(:idProducto) AND activo = true ORDER BY fecha_ingreso ASC")
 	public abstract List<Lote> findByLoteProductoActivo(long idProducto, long idLocal);
 
-	@Query("FROM Lote WHERE id_local = CASE(:idLocal) WHEN 0 THEN id_local ELSE (:idLocal) END AND id_producto = CASE(:idProducto) WHEN 0 THEN id_producto ELSE (:idProducto) END AND activo = CASE(:soloActivos) WHEN 0 THEN activo ELSE (true) END")
-	public abstract List<Lote> findByLocalProductoAndActivo(long idLocal, long idProducto, boolean soloActivos);
-	
 	// Devuelve una lista de lotes cuya cantidad actual no sea la inicial, es decir no nuevos
 	@Query("FROM Lote  WHERE id_local = (:idLocal) AND id_producto=(:idProducto) AND cantidad_inicial != cantidad_actual ORDER BY fecha_ingreso DESC")
 	public abstract List<Lote> findByLoteProductoNoNuevo(long idProducto, long idLocal);
 
+	public abstract List<Lote> findByLocalOrderByFechaIngresoDesc(Local local);
+
+	@Query("FROM Lote WHERE id_local = (:idLocal) AND " +
+	"id_producto = CASE(:idProducto) WHEN 0 THEN id_producto ELSE (:idProducto) END AND " + 
+	"activo = CASE(:soloActivos) WHEN 0 THEN activo ELSE (true) END " + 
+	"ORDER BY fecha_ingreso DESC")
+	public abstract List<Lote> findByALocalProductoAndActivo(long idLocal, long idProducto, boolean soloActivos);
 }
   
