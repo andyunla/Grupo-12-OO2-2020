da541ff1ca586091dbab704ca8a48dbd27fc71cc
andyunla <andy.dev.alnu@hotmail.com>
2020-06-17 15:17:34 -0300

    Pequeña modificación

--- src/main/java/com/sistema/application/configuration/SecurityConfiguration.java
+++ src/main/java/com/sistema/application/configuration/SecurityConfiguration.java
@@ -47,7 +47,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers(HttpMethod.GET, "/distancia/**").not().hasAuthority(UtilHelper.ROLE_ADMIN)
             .antMatchers(HttpMethod.GET, "/pedido/**").not().hasAuthority(UtilHelper.ROLE_ADMIN)
             .antMatchers(HttpMethod.GET, "/factura/todas**").not().hasAuthority(UtilHelper.ROLE_ADMIN)
-            .antMatchers(HttpMethod.GET, "/chango/**").not().hasAuthority(UtilHelper.ROLE_ADMIN)
+            .antMatchers(HttpMethod.GET, "/chango/**").hasAnyAuthority(UtilHelper.ROLE_EMPLEADO, UtilHelper.ROLE_GERENTE)
             .antMatchers(HttpMethod.GET, "/notificacion/**").not().hasAuthority(UtilHelper.ROLE_ADMIN)
             .antMatchers(HttpMethod.GET, "/local/**").hasAuthority(UtilHelper.ROLE_ADMIN)
             .antMatchers(HttpMethod.GET, "/lote/**").hasAuthority(UtilHelper.ROLE_GERENTE)
