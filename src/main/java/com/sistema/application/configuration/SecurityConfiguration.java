50e5d57921aa0ba05022eb097bb732bae2e6f378
andyunla <andy.dev.alnu@hotmail.com>
2020-06-17 14:24:33 -0300

    Agregado restricciones a los accesos a las diferentes URLs por los roles

--- src/main/java/com/sistema/application/configuration/SecurityConfiguration.java
+++ src/main/java/com/sistema/application/configuration/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.sistema.application.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +12,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.sistema.application.services.implementations.UserService;
+import com.sistema.application.helpers.ViewRouteHelper;
+import com.sistema.application.helpers.UtilHelper;
 
 @Configuration
 @EnableWebSecurity
@@ -36,6 +39,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
     	http.authorizeRequests()
             .antMatchers(resources).permitAll()
+            .antMatchers(ViewRouteHelper.HOME_ROOT).permitAll()
+            .antMatchers(HttpMethod.GET, "/empleado/**").not().hasAuthority("EMPLEADO")
+            .antMatchers(HttpMethod.GET, "/producto/**").not().hasAuthority("EMPLEADO")
+            .antMatchers(HttpMethod.GET, "/sueldos/**").not().hasAuthority("EMPLEADO")
+            .antMatchers(HttpMethod.GET, "/ranking/**").not().hasAuthority("EMPLEADO")
+            .antMatchers(HttpMethod.GET, "/distancia/**").not().hasAuthority("ADMIN")
+            .antMatchers(HttpMethod.GET, "/pedido/**").not().hasAuthority("ADMIN")
+            .antMatchers(HttpMethod.GET, "/factura/todas**").not().hasAuthority("ADMIN")
+            .antMatchers(HttpMethod.GET, "/chango/**").not().hasAuthority("ADMIN")
+            .antMatchers(HttpMethod.GET, "/local/**").hasAuthority("ADMIN")
+            .antMatchers(HttpMethod.GET, "/lote/**").hasAuthority("GERENTE")
+            .antMatchers(HttpMethod.GET, "/reporte/**").hasAuthority("GERENTE")
+            .antMatchers(HttpMethod.GET, "/items/**").hasAuthority("GERENTE")
+            .antMatchers(HttpMethod.GET, "/chango/todos**").hasAuthority("GERENTE")
             .anyRequest().authenticated()
         .and()
             .formLogin().loginPage("/login").loginProcessingUrl("/loginprocess")
