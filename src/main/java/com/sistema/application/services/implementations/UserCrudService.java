fed82858e2413b392d69304b3e6c0bd9ba06a9fa
andyunla <49526728+andyunla@users.noreply.github.com>
2020-06-17 22:15:14 -0300

    Removiendo código viejo

--- src/main/java/com/sistema/application/services/implementations/UserCrudService.java
+++ src/main/java/com/sistema/application/services/implementations/UserCrudService.java
@@ -1,29 +1,16 @@
 package com.sistema.application.services.implementations;
 
 import java.util.List;
-import java.util.Optional;
 
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.sistema.application.exception.CustomeFieldValidationException;
 import com.sistema.application.exception.UsernameOrIdNotFound;
 import com.sistema.application.dto.ChangePasswordForm;
 import com.sistema.application.entities.User;
-import com.sistema.application.repositories.IUserRepository;
-import com.sistema.application.services.IUserService;
+import com.sistema.application.services.IUserCrudService;
 
 @Service
-public class UserCrudService implements IUserService {
+public class UserCrudService implements IUserCrudService {
 
 	@Override
 	public List<User> getAllUsers() {
@@ -60,149 +47,4 @@ public class UserCrudService implements IUserService {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-	/*
-	@Autowired
-	@Qualifier("userRepository")
-	private IUserRepository userRepository;
-	
-	@Autowired
-	BCryptPasswordEncoder bCryptPasswordEncoder;
-	
-	@Override
-	public List<User> getAllUsers() {
-		return userRepository.findAll();
-	}
-	
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		com.sistema.application.entities.User user = userRepository.findByUsernameAndFetchUserRolesEagerly(username);
-		return buildUser(user, buildGrantedAuthorities(user.getUserRoles()));
-	}
-	
-	private User buildUser(com.sistema.application.entities.User user, List<GrantedAuthority> grantedAuthorities) {
-		return new User(user.getUsername(), user.getPassword(), user.isEnabled(),
-						true, true, true, //accountNonExpired, credentialsNonExpired, accountNonLocked,
-						grantedAuthorities);
-	}
-	
-	private List<GrantedAuthority> buildGrantedAuthorities(Set<UserRole> userRoles) {
-		Set<GrantedAuthority> grantedAuthorities = new HashSet<GrantedAuthority>();
-		for(UserRole userRole: userRoles) {
-			grantedAuthorities.add(new SimpleGrantedAuthority(userRole.getRole()));
-		}
-		return new ArrayList<GrantedAuthority>(grantedAuthorities);
-	}
-
-	private boolean checkUsernameAvailable(User user) throws Exception {
-		Optional<User> userFound = userRepository.findByUsername(user.getUsername());
-		if (userFound.isPresent()) {
-			throw new CustomeFieldValidationException("Username no disponible", "username");
-		}
-		return true;
-	}
-
-	private boolean checkPasswordValid(User user) throws Exception {
-		if (user.getConfirmPassword() == null || user.getConfirmPassword().isEmpty()) {
-			throw new CustomeFieldValidationException("Confirm Password es obligatorio","confirmPassword");
-		}
-		
-		if ( !user.getPassword().equals(user.getConfirmPassword())) {
-			throw new CustomeFieldValidationException("Password y Confirm Password no son iguales","password");
-		}
-		return true;
-	}
-
-
-	@Override
-	public User createUser(User user) throws Exception {
-		if (checkUsernameAvailable(user) && checkPasswordValid(user)) {
-			String encodedPassword = bCryptPasswordEncoder.encode(user.getPassword());
-			user.setPassword(encodedPassword);
-			user = userRepository.save(user);
-		}
-		return user;
-	}
-
-	@Override
-	public User getUserById(Long id) throws UsernameOrIdNotFound {
-		return userRepository.findById(id).orElseThrow(() -> new UsernameOrIdNotFound("El Id del usuario no existe."));
-	}
-
-	@Override
-	public User updateUser(User fromUser) throws Exception {
-		User toUser = getUserById(fromUser.getId());
-		mapUser(fromUser, toUser);
-		return userRepository.save(toUser);
-	}
-	
-	protected void mapUser(User from, User to) {
-		to.setUsername(from.getUsername());
-		to.setEmail(from.getEmail());
-		to.setRoles(from.getRoles());
-	}
-
-	@Override
-	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
-	public void deleteUser(Long id) throws UsernameOrIdNotFound {
-		User user = getUserById(id);
-		userRepository.delete(user);
-	}
-
-	@Override
-	public User changePassword(ChangePasswordForm form) throws Exception {
-		User user = getUserById(form.getId());
-		
-		if ( !isLoggedUserADMIN() && !user.getPassword().equals(form.getCurrentPassword())) {
-			throw new Exception ("Current Password invalido.");
-		}
-		
-		if( user.getPassword().equals(form.getNewPassword())) {
-			throw new Exception ("Nuevo debe ser diferente al password actual.");
-		}
-		
-		if( !form.getNewPassword().equals(form.getConfirmPassword())) {
-			throw new Exception ("Nuevo Password y Confirm Password no coinciden.");
-		}
-		
-		String encodePassword = bCryptPasswordEncoder.encode(form.getNewPassword());
-		user.setPassword(encodePassword);
-		return userRepository.save(user);
-	}
-	
-	private boolean isLoggedUserADMIN() {
-		//Obtener el usuario logeado
-		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		
-		UserDetails loggedUser = null;
-		Object roles = null;
-
-		//Verificar que ese objeto traido de sesion es el usuario
-		if (principal instanceof UserDetails) {
-			loggedUser = (UserDetails) principal;
-
-			roles = loggedUser.getAuthorities().stream()
-					.filter(x -> "ROLE_ADMIN".equals(x.getAuthority())).findFirst()
-					.orElse(null); 
-		}
-		return roles != null ? true : false;
-	}
-	
-	private User getLoggedUser() throws Exception {
-		//Obtener el usuario logeado
-		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		
-		UserDetails loggedUser = null;
-
-		//Verificar que ese objeto traido de sesion es el usuario
-		if (principal instanceof UserDetails) {
-			loggedUser = (UserDetails) principal;
-		}
-		
-		User myUser = userRepository
-				.findByUsername(loggedUser.getUsername()).orElseThrow(() -> new Exception("Error obteniendo el usuario logeado desde la sesion."));
-		
-		return myUser;
-	}
-	*/
 }
